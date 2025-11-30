0ec362903e6468e7505a0eaf5da1d8577a9a0788
Harjot <hsaini128@insite.4cd.edu>
2025-11-30 12:29:25 -0800

    add openDB function to initialize IndexedDB and create object store on first load

--- script.js
+++ script.js
@@ -316,6 +316,32 @@ function renderPlaylist(tracks, vibe, artistsUsed) {
   `;
 }
 
+//------------------------------
+// 6) INDEXEDDB: SAVE / LOAD LAST PAIRING
+//------------------------------
+
+const DB_NAME = "mashupDB";
+const DB_VERSION = 1;
+const STORE_NAME = "pairings";
+
+function openDB() {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(DB_NAME, DB_VERSION);
+
+    request.onupgradeneeded = (event) => {
+      const db = event.target.result;
+      if (!db.objectStoreNames.contains(STORE_NAME)) {
+        db.createObjectStore(STORE_NAME, { keyPath: "id" });
+      }
+    };
+
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject(request.error);
+  });
+}
+
+
+
 // -----------------------------
 // 5) MAIN FLOW
 // -----------------------------
